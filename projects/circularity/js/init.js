22aeaab627653d0b632f6a45317c312d1fec7898
SandroRRamos <princereku21@gmail.com>
2022-12-20 14:40:34 +0000

    circle

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,12 +20,23 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
-
+        var circle;
+        var circles = [];
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle(){
+
+        circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+        physikz.addRandomVelocity(circle, canvas, 5, 5);
+        view.addChild(circle);
+        circles.push(circle);
+
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
+        
+        for (var draw = 0; draw < 100; draw++){
+            drawCircle();
+        }
 
 
         ////////////////////////////////////////////////////////////
@@ -40,12 +51,18 @@ var init = function (window) {
         function update() {
             // TODO 4 : Update the circle's position //
 
+            for (var i = 0; i < 100; i++){
+            physikz.updatePosition( circles[i] );
+            }
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
+            for (var j = 0; j < 100; j++){
+            game.checkCirclePosition( circles[j] );
+            }
            
 
             // TODO 9 : Iterate over the array
-           
+            
             
         }
     
@@ -57,13 +74,25 @@ var init = function (window) {
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
+            if ( circle.x > canvas.width) {
                 circle.x = 0;
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             
 
+            // if the circle goes past the top of the screen then place it at the bottom
+            if ( circle.y > canvas.height){
+                circle.y = 0;
+            }
+            // if the circle goes past the bottom of the screen then place it at the top
+            if ( circle.y < 0){
+                circle.y = canvas.height;
+            }
+            // if the circle goes past the left of the screen then place it at the right
+            if ( circle.x < 0){
+                circle.x = canvas.width;
+            }
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
